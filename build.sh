79aba73029777ba54f87324205d5675dbb87c981
damiva <96115589+damiva@users.noreply.github.com>
2022-01-24 01:31:41 +0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ for d in *; do
     if [ -d "$d" ]; then
         echo -e "\n> $d.plug:"
         cd $d
-        tar -cvzf ../$d.200.tgz *
+        tar -cvzf ../$d.201.tgz *
         cd ..
     fi
 done
