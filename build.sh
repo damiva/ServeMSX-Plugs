ad21dde412249b882c65d1689f89c479c83aea48
damiva <96115589+damiva@users.noreply.github.com>
2022-02-01 18:27:03 +0300

    Add files via upload
    - tivix works with ffmpeg HLS to MPEGTS conversion
    - torlook in two languages (Russian & English)

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ for d in *; do
     if [ -d "$d" ]; then
         echo -e "\n> $d.plug:"
         cd $d
-        tar -cvzf ../$d.201.tgz *
+        tar -cvzf ../$d.203.tgz *
         cd ..
     fi
 done
